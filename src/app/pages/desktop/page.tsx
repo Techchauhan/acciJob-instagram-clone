1937caa829e31bfac65671fcdfc9af2ecdb12410
Techchauhan <rishabkumarchauhan@gmail.com>
2024-11-30 14:09:45 +0530

    shift the layout according to the size

---
+++ src/app/pages/desktop/page.tsx
@@ -0,0 +1,5 @@
+const DesktopScreen = () => (
+    <div className="text-center text-2xl">This is the Desktop View</div>
+);
+
+  export default DesktopScreen;
---
+++ src/app/pages/mobile/page.tsx
@@ -0,0 +1,5 @@
+const MobileScreen = () => (
+    <div className="text-center text-xl">This is the Mobile View</div>
+  );
+
+  export default MobileScreen
